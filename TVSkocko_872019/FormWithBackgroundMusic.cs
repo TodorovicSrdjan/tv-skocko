6a9e344ec1c96ccd0100c9359091163d57fdcd29
TodorovicSrdjan <tsrdjan@pm.me>
2022-09-05 02:02:18 +0200

    fix: use right resource for sonfInfo title

--- TVSkocko_872019/FormWithBackgroundMusic.cs
+++ TVSkocko_872019/FormWithBackgroundMusic.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +16,7 @@ namespace TVSkocko_872019
 
         public FormWithBackgroundMusic()
         {
-            songInfoInWindowTitle = $"{Resources.Background}    ";
+            songInfoInWindowTitle = $"{Resources.BackgroundSongInfo}    ";
             this.Text = $"{Resources.WindowTitle}: {songInfoInWindowTitle}";
             InitializeTimer();
         }
