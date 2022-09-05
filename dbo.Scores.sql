cd41514b03a6efc77be0fd9b7fd0b7042d2da0e4
TodorovicSrdjan <tsrdjan@pm.me>
2022-09-05 22:56:20 +0200

    feat: add sql code for creating table Scores

---
+++ dbo.Scores.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Scores] (
+    [id]                   INT           IDENTITY (1, 1) NOT NULL,
+    [player_name]          NVARCHAR (20) NOT NULL,
+    [game_duration]        INT           NOT NULL,
+    [number_of_guesses]    SMALLINT      NOT NULL,
+    [has_guessed_solution] BIT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
